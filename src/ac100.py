1ac5b92cd6eadefe56e970c2362d3e79f796a2fd
Andrew Chung <andrewtc4@gmail.com>
2022-12-14 21:30:48 -0500

    Start check_video_dimensions()

--- src/ac100.py
+++ src/ac100.py
@@ -31,6 +31,36 @@ class AC100:
         self.VIDEO_HEIGHT: int = defs.DEFAULT_VIDEO_ROWS
         self.VRAM_START: int = defs.DEFAULT_VRAM_START
 
+
+def check_video_dimensions(args) -> [int]:
+    """
+    Ensure video dimensions are valid
+
+    Make sure video dimensions
+    a: Aren't negative: having negative dimensions makes no sense!
+    b: Don't cause VRAM to encroach on stack portion of memory
+
+    We don't have to check explicitly check if VRAM > total RAM, because if the
+    VRAM encroaches on the stack, then there's no general-purpose RAM
+    left anyways
+
+    Parameters:
+    args: the args from the argument parser; check if the user requested
+    custom dimensions and, if so, make sure they're valid
+
+    Return:
+    The final dimensions to use for VRAM allocation.  The first value is the
+    video height; the second value is the video width
+
+    If either user-supplied dimension is negative, raise a
+    NegativeVideoDimensionError
+
+    If either user-supplied dimension would cause the VRAM to encroach on the
+    stack, raise a VRAMTooLargeError
+    """
+    dimensions: [int] = [defs.DEFAULT_VIDEO_ROWS, defs.DEFAULT_VIDEO_COLUMNS]
+
+
 def setup_parser(parser):
     parser.add_argument("binary", help="AC100 binary to run")
     parser.add_argument("-c", "--columns", default=defs.DEFAULT_VIDEO_COLUMNS,
