7850231774d1e163231feebf37c2f68818fa342d
Andrew Chung <andrewtc4@gmail.com>
2022-12-15 16:59:34 -0500

    test_asm: Rename, regroup tests
    Makes the test names shorter, so reports are easier to read

--- tests/test_asm.py
+++ tests/test_asm.py
@@ -22,32 +22,36 @@ class TestAssembler:
         assert result == expected, f"Should have gotten {expected} when "\
             f"tokenizing '{line}', but got {result}"
 
+
+class TestParseRegisterName:
     @pytest.mark.parametrize("name, expected", [("R1", 0), ("R2", 1), ("R3", 2),
                             ("R4", 3), ("R5", 4), ("R6", 5), ("R7", 6),
                             ("R8", 7), ("R9", 8), ("R10", 9), ("R11", 10),
                             ("R12", 11), ("R13", 12), ("R14", 13), ("R15", 14),
                             ("R16", 15)])
-    def test_parse_register_name_valid(self, name, expected):
+    def test_valid_register(self, name, expected):
         parsed = assembler.parse_register_name(name)
         assert parsed == expected, f"Got value {parsed} while parsing {name}, "\
             f"but expected {expected}"
 
-    def test_parse_register_name_no_prefix(self):
+    def test_no_register_prefix(self):
         token = "1"             # valid number, but no prefix
         with pytest.raises(ac_exc.RegisterNameMissingPrefixError):
             assembler.parse_register_name(token)
 
-    def test_parse_register_name_too_low(self):
+    def test_register_name_too_low(self):
         token = "R0"
         with pytest.raises(ac_exc.InvalidRegisterNameError):
             assembler.parse_register_name(token)
 
-    def test_parse_register_name_too_high(self):
+    def test_register_name_too_high(self):
         token = "R20"
         with pytest.raises(ac_exc.InvalidRegisterNameError):
             assembler.parse_register_name(token)
 
-    def test_parse_int_decimal(self):
+
+class TestParseInt:
+    def test_decimal(self):
         token = "-65536"        # too negative for 16 bits
         number = assembler.parse_int(token)
         assert number is None, "Should have failed on too-negative value "\
@@ -79,15 +83,14 @@ class TestAssembler:
     @pytest.mark.parametrize("token, expected",
                              [("0x0", b"\x00"), ("0xf3", b"\xf3"),
                               ("0x0000", b"\x00\x00"), ("0xffff", b"\xff\xff")])
-    def test_parse_int_hex_valid(self, token, expected):
+    def test_valid_hex(self, token, expected):
 
         number = assembler.parse_int(token)
         assert number == expected,\
             f"Should have gotten {expected} on token {token}, but got "\
             f"{number}"
 
-    def test_parse_int_hex_invalid(self):
-
+    def test_invalid_hex(self):
         token = "0xg"           # invalid hex digit
         with pytest.raises(ValueError):
             assembler.parse_int(token)
@@ -110,12 +113,12 @@ class TestAssembler:
                               ("0b11111111", b"\x00\xff"),
                               ("0b0000000000000000", b"\x00\x00"),
                               ("0b1111111111111111", b"\xff\xff")])
-    def test_parse_int_binary_valid(self, token, expected):
+    def test_valid_binary(self, token, expected):
         number = assembler.parse_int(token)
         assert number == expected, f"Expected {expected} for token {token}, "\
             f"but got {number}"
 
-    def test_parse_int_binary_invalid(self):
+    def test_invalid_binary(self):
         token = "0b"
         with pytest.raises(ValueError):
             number = assembler.parse_int(token)
@@ -130,15 +133,17 @@ class TestAssembler:
         with pytest.raises(ValueError):
             number = assembler.parse_int(token)
 
+
+class TestParseAddress:
     @pytest.mark.parametrize("token, expected",
                              [("0x0000", b"\x00\x00"), ("0xffff", b"\xff\xff"),
                               ("0xbeef", b"\xbe\xef"), ("0xdead", b"\xde\xad")])
-    def test_parse_address_valid(self, token, expected):
+    def test_valid_address(self, token, expected):
         address = assembler.parse_address(token)
         assert address == expected, f"Expected {expected} for token "\
             f"'{token}', but got {address}"
 
-    def test_parse_address_invalid(self):
+    def test_invalid_address(self):
         # valid hex number, but missing prefix, so would be interpreted as
         # decimal, which we’re disallowing
         token = "1234"
