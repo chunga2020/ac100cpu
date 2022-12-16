95d46c5a734e4fc4f3746cf286fc0d02035656e2
Andrew Chung <andrewtc4@gmail.com>
2022-12-16 12:34:38 -0500

    Add first test of assembler failure

---
+++ tests/test_asm_instructions_fail.py
@@ -0,0 +1,17 @@
+import pathlib
+import pytest
+
+import src.ac100asm as asm
+import src.exceptions as ac_exc
+
+assembler = asm.AC100ASM()
+test_srcd = pathlib.Path("asm_tests_failing")
+ldi_tests = pathlib.Path(test_srcd, "ldi_tests")
+
+class TestLdiFailures:
+    def test_ldi_bad_register(self):
+        source_file = pathlib.Path(ldi_tests, "test01")
+        with open(source_file, "r") as f:
+            bytecode = assembler.assemble(f)
+            assert bytecode is None,\
+                "Assembly should have failed with bad register name"
