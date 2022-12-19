7a9cfafc4af8fb76505e989afc3688b11013e66f
Andrew Chung <andrewtc4@gmail.com>
2022-12-18 20:35:58 -0500

    asm: Add JG fails if destination in stack test

--- tests/test_asm_instructions_fail.py
+++ tests/test_asm_instructions_fail.py
@@ -125,6 +125,7 @@ class TestJumpFailures:
             ("test03", "JE assembly should fail if destination is in the stack"),
             ("test04", "JG assembly should fail if address is missing prefix"),
             ("test05", "JG assembly should fail if address not 16 bits"),
+            ("test06", "JG assembly should fail if destination is in the stack")
         ])
     def test_jump_failures(self, src_file, assert_msg):
         with open(pathlib.Path(jump_tests, src_file), "r") as f:
